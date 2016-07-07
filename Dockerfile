7899ce8e2b29fbbdcefb2fb11946588f7a2940b0
Rene Meyer <rene.meyer@de.ibm.com>
2016-07-07 13:54:42 +0200

    cleanup 2

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,21 @@
 FROM registry.eu-gb.bluemix.net/ibmnode:latest
 RUN mkdir /var/www
 VOLUME /var/www/
-RUN mkdir /myapp
-COPY ./* /myapp/
+
+RUN mkdir /myapp/
+RUN mkdir /myapp/client
+RUN mkdir /myapp/views
+
+COPY ./client /myapp/client
+COPY ./views /myapp/views
+
+COPY .bowerrc /myapp/
+COPY ./app.js /myapp/
+COPY ./bower.json /myapp/
+COPY ./package.json /myapp/
+COPY ./routes.js /myapp/
+COPY ./start.sh /myapp/
+
 WORKDIR /myapp
 RUN npm install
 EXPOSE 8000
--- app.js
+++ app.js
@@ -5,19 +5,32 @@ var request = require('request');
 var url = require('url');
 var fs = require("fs");
 var mime = require('mime-types');
-var program = require('commander');
-var _ = require('lodash');
 
-var globalpath = '/var/www/';
-var port = 8000;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var express = require('express');
 var app = express();
-var dir =  process.cwd();
+var routes = require('./routes.js');
+
+
+var globalpath = '/var/www';
+var port = 8000;
+
 
 app.use(busboy());
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname)); //module directory
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, './client')));
+app.use('/', routes);
+app.locals.globalpath = globalpath;
+
 var server = http.createServer(app);
 server.listen(port);
 
@@ -42,14 +55,6 @@ app.route('/upload')
         });
 
     });
-
-String.prototype.startsWith = function(prefix) {
-    return this.indexOf(prefix) === 0;
-}
-
-String.prototype.endsWith = function(suffix) {
-    return this.match(suffix+"$") == suffix;
-};
 
 function doesExist(filePath) {
 	  try {
@@ -94,213 +99,3 @@ app.get('/download/*', function (req, res) {
 		});
 	}
 });
-
-app.get('/files', function(req, res) {
- var currentDir =  globalpath;
- var query = req.query.path || '';
- if (query) currentDir = path.join(globalpath, query);
- console.log("browsing ", currentDir);
- fs.readdir(currentDir, function (err, files) {
-     if (err) {
-        throw err;
-      }
-      var data = [];
-      files
-      .filter(function (file) {
-          return true;
-      }).forEach(function (file) {
-        try {
-                //console.log("processing ", file);
-                var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
-                if (isDirectory) {
-                  data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
-                } else {
-                  var ext = path.extname(file);
-                  if(program.exclude && _.contains(program.exclude, ext)) {
-                    console.log("excluding file ", file);
-                    return;
-                  }
-                  data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
-                }
-
-        } catch(e) {
-          console.log(e);
-        }
-
-      });
-      data = _.sortBy(data, function(f) { return f.Name });
-      res.json(data);
-  });
-});
-
-app.get('/', function(req, res) {
- res.redirect('public/template.html');
-});
-
-
-/*
-router.addRoute('/download*', function (req, res, params) {
-
-		var uri = url.parse(req.url).pathname;
-		var file = path.basename(uri);
-		var filePath = path.join(globalpath, file);
-		console.log(filePath);
-		var stat = null;
-		try{
-	    	stat = fs.statSync(filePath);
-		}
-		catch(err) {
-			console.log('Something bad happened');
-		}
-
-		if(stat!=null)
-	    {
-	    	contentType = "";
-	        if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) {
-	          contentType = "image/jpeg";
-	        } else if (filePath.endsWith(".gif")) {
-	          contentType = "image/gif";
-	        } else if (filePath.endsWith(".png")) {
-	          contentType = "image/png";
-	        } else if (filePath.endsWith(".bmp")) {
-	          contentType = "image/bmp";
-	        } else if (filePath.endsWith(".pdf")) {
-	          contentType = "application/pdf";
-	        } else if (filePath.endsWith(".mp4")) {
-	          contentType = "video/mp4";
-	    	}
-	        else {
-	          contentType = "text/plain";
-	        }
-		    res.writeHead(200, {
-		        'Content-Type': contentType,
-		        'Content-Length': stat.size
-		    });
-
-		    var readStream = fs.createReadStream(filePath);
-		    readStream.on('data', function(data) {
-		        res.write(data);
-		        console.log('download chunk');
-		    });
-
-		    readStream.on('end', function() {
-		        res.end();
-		        console.log('download ready ' + filePath);
-		    });
-	    }
-	    else
-	    {
-	    	res.writeHead(404);
-	    	res.end();
-	    	console.log('File not found:' + filePath);
-	    }
-});*/
-
-
-
-
-
-
-// // Example from http://schempy.com/2015/03/11/streaming_file_uploads_with_nodejs/
-// // Define our route for uploading files
-// router.addRoute('/upload', function (req, res, params) {
-// 	if (req.method === 'POST') {
-//
-// 		// Create an Busyboy instance passing the HTTP Request headers.
-// 		var busboy = new Busboy({ headers: req.headers });
-//
-// 		// Listen for event when Busboy finds a file to stream.
-// 		busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-// 			var filePath = path.join(globalpath, filename);
-// 			console.log(filePath);
-// 			fileStream = fs.createWriteStream(filePath);
-// 			// We are streaming! Handle chunks
-// 			file.on('data', function (data) {
-// 				// Here we can act on the data chunks streamed.
-// 				console.log('Parts received ' + fieldname);
-// 				fileStream.write(data, "binary");
-// 			});
-//
-// 			// Completed streaming the file.
-// 			file.on('end', function () {
-// 				console.log('Finished with ' + fieldname);
-// 				fileStream.addListener("drain", function() {
-// 		            // Close file stream
-// 		            fileStream.end();
-// 		            // Handle request completion, as all chunks were already written
-// 		        });
-// 			});
-// 		});
-//
-// 		// Listen for event when Busboy finds a non-file field.
-// 		busboy.on('field', function (fieldname, val) {
-// 			// Do something with non-file field.
-// 		});
-//
-// 		// Listen for event when Busboy is finished parsing the form.
-// 		busboy.on('finish', function () {
-// 			res.statusCode = 200;
-// 			res.end();
-// 		});
-//
-// 		// Pipe the HTTP Request into Busboy.
-// 		req.pipe(busboy);
-// 	}
-// });
-
-// router.addRoute('/download*', function (req, res, params) {
-//
-// 		var uri = url.parse(req.url).pathname;
-// 		var file = path.basename(uri);
-// 		var filePath = path.join(globalpath, file);
-// 		console.log(filePath);
-// 		var stat = null;
-// 		try{
-// 	    	stat = fs.statSync(filePath);
-// 		}
-// 		catch(err) {
-// 			console.log('Something bad happened');
-// 		}
-//
-// 		if(stat!=null)
-// 	    {
-// 	    	contentType = "";
-// 	        if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) {
-// 	          contentType = "image/jpeg";
-// 	        } else if (filePath.endsWith(".gif")) {
-// 	          contentType = "image/gif";
-// 	        } else if (filePath.endsWith(".png")) {
-// 	          contentType = "image/png";
-// 	        } else if (filePath.endsWith(".bmp")) {
-// 	          contentType = "image/bmp";
-// 	        } else if (filePath.endsWith(".pdf")) {
-// 	          contentType = "application/pdf";
-// 	        } else if (filePath.endsWith(".mp4")) {
-// 	          contentType = "video/mp4";
-// 	    	}
-// 	        else {
-// 	          contentType = "text/plain";
-// 	        }
-// 		    res.writeHead(200, {
-// 		        'Content-Type': contentType,
-// 		        'Content-Length': stat.size
-// 		    });
-//
-// 		    var readStream = fs.createReadStream(filePath);
-// 		    readStream.on('data', function(data) {
-// 		        res.write(data);
-// 		        console.log('download chunk');
-// 		    });
-//
-// 		    readStream.on('end', function() {
-// 		        res.end();
-// 		        console.log('download ready ' + filePath);
-// 		    });
-// 	    }
-// 	    else
-// 	    {
-// 	    	res.writeHead(404);
-// 	    	res.end();
-// 	    	console.log('File not found:' + filePath);
-// 	    }
-// });
